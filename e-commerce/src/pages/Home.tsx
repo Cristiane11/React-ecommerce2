6df45db6151f86de5f750ac7b5c2b80f8b9fe830
Cristiane Abreu <bjwmcd@yahoo.com>
2025-11-17 18:27:57 -0500

    update

--- e-commerce/src/pages/Home.tsx
+++ e-commerce/src/pages/Home.tsx
@@ -1,10 +1,36 @@
-import React from 'react'
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { Container } from 'react-bootstrap';
 
 const Home = () => {
+
+    
+    interface Product{
+       id: number; 
+       title: string;
+       price: number;
+       description: string;
+       category:string;
+       image:string;
+       rating:{
+         rate:number;
+         count:number;
+       }
+    }
+    const [prodcts, setProducts] = useState<Product[]>([]);
+   
+    useEffect(()=>{
+        const fetchProducts = async () =>{
+            const response = await fetch('https://fakestoreapi.com/products');
+            const data =  await response.json();
+        }
+        fetchProducts()
+    },[])
   return (
-    <div>
-      Home
-    </div>
+     <Container className="mt-5">
+        Home
+     </Container>
+    
   )
 }
 
