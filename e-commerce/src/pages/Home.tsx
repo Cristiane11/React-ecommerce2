4c5597ed867d97a8790c4db8b540e458c0b337da
Cristiane Abreu <bjwmcd@yahoo.com>
2025-11-19 11:37:38 -0500

    update

--- e-commerce/src/pages/Home.tsx
+++ e-commerce/src/pages/Home.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
-import { Container } from 'react-bootstrap';
 
-const Home = () => {
+import { useState, useEffect } from 'react';
+import { Container, Card } from 'react-bootstrap';
+import '../index.css'
+
+ const Home = () => {
 
     
     interface Product{
@@ -17,21 +18,34 @@ const Home = () => {
          count:number;
        }
     }
-    const [prodcts, setProducts] = useState<Product[]>([]);
+    const [products, setProducts]  = useState<Product[]>([]);
    
     useEffect(()=>{
         const fetchProducts = async () =>{
             const response = await fetch('https://fakestoreapi.com/products');
             const data =  await response.json();
+            setProducts(data)
         }
         fetchProducts()
     },[])
   return (
      <Container className="mt-5">
-        Home
+          {products.map((product:Product)=>(
+            <Card>
+              <Card.Img src={product.image} />
+              <Card.Body>
+                <Card.Title>{product.title}</Card.Title>
+                <h3>{product.price}</h3>
+              </Card.Body>
+                
+            </Card>
+            
+          ))}
+          
      </Container>
     
   )
-}
+};
+export default Home;
 
-export default Home
+
