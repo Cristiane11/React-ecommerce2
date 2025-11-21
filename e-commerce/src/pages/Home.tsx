dcaeaf91c08ae6871f9d678b37af56d3db4689ed
Cristiane Abreu <bjwmcd@yahoo.com>
2025-11-21 14:27:34 -0500

    update

--- e-commerce/src/pages/Home.tsx
+++ e-commerce/src/pages/Home.tsx
@@ -4,21 +4,23 @@ import ProductCard from '../components/ProductCard';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from '../components/NavBar';
 import type { Product } from '../types/types';
+import  {useProductContext} from '../context/ProductContext';
 //import '../index.css'
 
  const Home:React.FC = () => {
 
-   
-    const [products, setProducts]  = useState<Product[]>([]);
+   const {products, dispatch} = useProductContext();
+    //const [products, setProducts]  = useState<Product[]>([]);
    
     useEffect(()=>{
         const fetchProducts = async () =>{
             const response = await fetch('https://fakestoreapi.com/products');
             const data =  await response.json();
-            setProducts(data)
+            dispatch({type:'SET_PRODUCT', payload: data});
+           // setProducts(data)
         }
         fetchProducts()
-    },[])
+    },[dispatch])
             return (
               <>
               <NavBar/>
--- e-commerce/src/pages/Profile.tsx
+++ e-commerce/src/pages/Profile.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import  {useProductContext} from '../context/ProductContext';
+import type { Product } from '../types/types';
 
 const Profile :React.FC = () => {
+   const {products, selectedCategory, dispatch} = useProductContext();
   return (
     <div>
-      
+      {products.map((product: Product)=>(
+        <div key={product.id}>
+          <h1>{product.title}</h1>
+          <p>{product.image}</p>
+        </div>
+      ))}
+
     </div>
   )
 }
